7d4df0d2d6d70bd80801ca11bdefdd3e93a01c62
konturn <konturn@URB218-20.seasad.wustl.edu>
2018-01-17 17:02:41 -0600

    this is a commit message

---
+++ heartbeat/heartbeat/heartbeat.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+int x=0;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.print(x);
+Serial.println(" seconds have elapsed.");
+delay(1000);
+x=x+1000;
+}
