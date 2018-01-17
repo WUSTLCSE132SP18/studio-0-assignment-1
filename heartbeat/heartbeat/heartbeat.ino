f40a9528e5a5ea0ed4b8bd496f82559d7f40ad6f
konturn <konturn@URB218-20.seasad.wustl.edu>
2018-01-17 17:21:14 -0600

    this is a commit message

--- heartbeat/heartbeat/heartbeat.ino
+++ heartbeat/heartbeat/heartbeat.ino
@@ -1,13 +1,17 @@
+double x=0;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
-int x=0;
+pinMode(13,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 Serial.print(x);
+digitalWrite(13,HIGH);
 Serial.println(" seconds have elapsed.");
-delay(1000);
-x=x+1000;
+delay(500);
+digitalWrite(13, LOW);
+delay(500);
+x=millis()/1000.0;
 }
